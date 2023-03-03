3d758e602408e491ccba1db38828be4b4622e94d
Mohanapriyasathish <mohanapriyabalu14@gmail.com>
2023-03-03 15:44:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+setTimeout(()=>{
+   document.getElementById("container").innerHTML="10"
+    setTimeout(()=>{
+        document.getElementById("container").innerHTML="9"
+        setTimeout(()=>{
+            document.getElementById("container").innerHTML="8"
+            setTimeout(()=>{
+                document.getElementById("container").innerHTML="7"
+                setTimeout(()=>{
+                    document.getElementById("container").innerHTML="6"
+                    setTimeout(()=>{
+                        document.getElementById("container").innerHTML="5"
+                        setTimeout(()=>{
+                            document.getElementById("container").innerHTML="4"
+                            setTimeout(()=>{
+                                document.getElementById("container").innerHTML="3"
+                                setTimeout(()=>{
+                                    document.getElementById("container").innerHTML="2"
+                                    setTimeout(()=>{
+                                        document.getElementById("container").innerHTML="1"
+                                        setTimeout(()=>{
+                                            document.getElementById("container").innerHTML="Happy Independence Day"
+        
+    
+
+
+
+
+
+
+        
+        
+
+        
+                                
+            },1000)
+              
+},1000)
+
+},1000)
+ },1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+
+
+
